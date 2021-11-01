3fd369aa9f0d885988967de2aae1402e28b288d9
KM <40517079+nisinosora@users.noreply.github.com>
2021-11-01 09:22:32 +0900

    バグ修正

--- index.js
+++ index.js
@@ -132,9 +132,9 @@ document.addEventListener('DOMContentLoaded',function(){
       
       var links = document.getElementById("canvas_2d").toDataURL('image/png')
       var link_img = document.getElementById("canvas_img")
-      link_img.css("width", myCanvas.width);
-      link_img.css("height", myCanvas.height);
       link_img.src = links;
+      link_img.width = myCanvas.width;
+      link_img.height = myCanvas.height;
       link_img.style.display = "inline"
     }else{
       alert("アイコンがありません")
