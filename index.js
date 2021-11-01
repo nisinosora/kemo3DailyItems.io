4a89c8c0e3cd0b580ff80e1aeb0306e7a18714b6
KM <40517079+nisinosora@users.noreply.github.com>
2021-11-01 09:18:16 +0900

    test_canvas_update5
    バグ修正

--- index.js
+++ index.js
@@ -132,8 +132,8 @@ document.addEventListener('DOMContentLoaded',function(){
       
       var links = document.getElementById("canvas_2d").toDataURL('image/png')
       var link_img = document.getElementById("canvas_img")
-      link_img.css("width", myCanvas.width)
-      link_img.css("height", myCanvas.height)
+      link_img.css("width", myCanvas.width);
+      link_img.css("height", myCanvas.height);
       link_img.src = links;
       link_img.style.display = "inline"
     }else{
