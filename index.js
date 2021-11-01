8d0673cc6eb3470b43b8d0e327483a61f99bf945
KM <40517079+nisinosora@users.noreply.github.com>
2021-11-01 09:01:10 +0900

    test_canvas_update
    canvasの画質向上テスト

--- index.js
+++ index.js
@@ -96,6 +96,19 @@ document.addEventListener('DOMContentLoaded',function(){
       var x_ind = 0, y_ind = 0;
       var arys = [], output_check;
       lists.clearRect(0, 0, 350, 350);
+
+      var myCanvas = document.getElementById("canvas_2d");
+
+      // 1. canvas要素のwidth属性とheight属性をdevicePixelRatio分だけ拡大する。
+      myCanvas.width *= devicePixelRatio;
+      myCanvas.height *= devicePixelRatio;
+
+      // 2. canvas要素のstyle属性のwidthとheightをdevicePixelRatio分だけ縮小する。
+      myCanvas.style.width =
+        String(myCanvas.width / devicePixelRatio) + "px";
+      myCanvas.style.height =
+        String(myCanvas.height / devicePixelRatio) + "px";
+        
       $("#lists").find('img').each(function(i){
         arys[i] = new Image()
         arys[i].src = $(this).attr('src')
