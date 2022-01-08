6e390e2f02c5dc05eb69bb8ef0f7d5777e4386ce
KM <40517079+nisinosora@users.noreply.github.com>
2022-01-08 09:08:42 +0900

    back
    back_state

--- index.js
+++ index.js
@@ -125,18 +125,18 @@ document.addEventListener('DOMContentLoaded',function(){
       "url": "https://nisinosora.github.io/kemo3DailyItems.io/",
       "hashtags": "ログボマトメールP"
     }
-    //window.open(`http://twitter.com/intent/tweet?text=${infos["text"]}&url=${infos["url"]}&hashtags=${infos["hashtags"]}`);
-    var links = document.getElementById("canvas_2d").toDataURL('image/png');
-    image = links;
-    navigator.share({
-      text: `${infos["text"]}`,
-      url: `${infos["url"]}`,
-      files: [image]
-    }).then(() => {
-      console.log('Share was successful.')
-    }).catch((error) => {
-      console.log('Sharing failed', error)
-    })
+    window.open(`http://twitter.com/intent/tweet?text=${infos["text"]}&url=${infos["url"]}&hashtags=${infos["hashtags"]}`);
+    // var links = document.getElementById("canvas_2d").toDataURL('image/png');
+    // image = links;
+    // navigator.share({
+    //   text: `${infos["text"]}`,
+    //   url: `${infos["url"]}`,
+    //   files: [image]
+    // }).then(() => {
+    //   console.log('Share was successful.')
+    // }).catch((error) => {
+    //   console.log('Sharing failed', error)
+    // })
   });
 
   //生成関数
