ff1dcf0f0756da0dc887f416d7b35c84d324638e
KM <40517079+nisinosora@users.noreply.github.com>
2024-03-02 16:34:58 +0900

    move to langlist

---
+++ script/localize_list.js
@@ -0,0 +1,274 @@
+const $alerts = {
+  "nullIcons":{
+    "ja": "削除するアイコンがありません", 
+    "zh-Hant": "沒有可刪除的圖標",
+    "zh-Hans":"没有可删除的图标",
+    "en":"No icon to delete"
+  },
+  "shareError":{
+    "ja": "エラーが発生しました。\n画像が存在しないか、ブラウザが非対応の可能性があります", 
+    "zh-Hant": "發生錯誤。\n圖片可能不存在或瀏覽器不支持",
+    "zh-Hans":"发生错误。 \n图片可能不存在或浏览器不支持",
+    "en":"Error. \nThe image may not exist or your browser may not support it."
+  },
+  "cantAddIcons":{
+    "ja": "これ以上はアイコンを追加できません",
+    "zh-Hant": "無法添加更多圖標",
+    "zh-Hans": "无法添加更多图标",
+    "en":"No more icons can be added"
+  },
+  "cantChoiceIcons":{
+    "ja": "「アイテム置換」欄にアイテムがないため選択できません",
+    "zh-Hant":"無法選中，因為「替換項目」中沒有可選物品",
+    "zh-Hans":"无法选中，因为「替换项目」中没有可选物品",
+    "en":"Cannot be selected because there is no item in the “Replace Item” field"
+  },
+  "AllIconsRemove":{
+    "ja":"全てのアイコンを削除します。よろしいですか？",
+    "zh-Hant":"刪除所有圖標。確定嗎？",
+    "zh-Hans":"删除所有图标。确定吗？",
+    "en":"Delete all icons. Is it OK?"
+  }
+}
+
+const $labels = {
+  "title":{
+    "ja": "【けものフレンズ3】ログボマトメールP", 
+    "zh-Hant":"【動物朋友3】全部都記下來M", 
+    "zh-Hans":"【动物朋友3】全部都记下来M",
+    "en":"《Kemono Friends 3》Mission Memo Master M"
+  },
+  "#Usage_PassportList":{
+    "ja": "リスト：パスポート", 
+    "zh-Hant":"列表：月卡",
+    "zh-Hans":"列表：月卡",
+    "en":"List: Pass"
+  },
+  "#Usage_ModeList":{
+    "ja":"リスト：モード", 
+    "zh-Hant":"列表：模式",
+    "zh-Hans":"列表：模式",
+    "en":"List: Mode"
+  },
+  "#Usage_Create_Download":{
+    "ja": "生成・ダウンロード", 
+    "zh-Hant":"生成、下載",
+    "zh-Hans":"生成、下载",
+    "en":"Generate / Download"
+  },
+  "#Usage_Remove":{
+    "ja": "削除",
+    "zh-Hant":"刪除",
+    "zh-Hans":"删除",
+    "en":"Delete"
+  },
+  "#Usage_Share":{
+    "ja":"共有",
+    "zh-Hant":"分享",
+    "zh-Hans":"分享",
+    "en":"Share"
+  },
+  "#Usage_EmissionRate":{
+    "ja":"排出率算出",
+    "zh-Hant":"計算概率",
+    "zh-Hans":"计算概率",
+    "en":"Calculate Drop Rate"
+  },
+  "#Usage_Other":{
+    "ja":"その他",
+    "zh-Hant":"其他",
+    "zh-Hans":"其他",
+    "en":"Other"
+  },
+  "#Label_Passport":{
+    "ja":"パスポート：",
+    "zh-Hant":"月卡：",
+    "zh-Hans":"月卡：",
+    "en":"Pass:"
+  },
+  "#Label_Mode":{
+    "ja":"モード：",
+    "zh-Hant":"模式：",
+    "zh-Hans":"模式：",
+    "en":"Mode:"
+  },
+  "#Label_result_list_filter":{
+    "ja":"フィルター：",
+    "zh-Hant":"篩選：",
+    "zh-Hans":"筛选：",
+    "en":"Filter:"
+  },
+  "#HowToUse":{
+    "ja":"使用方法：",
+    "zh-Hant":"使用方法：",
+    "zh-Hans":"使用方法：",
+    "en":"How To Use:"
+  }
+}
+
+const $options = {
+  "option_all":{
+    "ja":"すべて",
+    "zh-Hant":"全部",
+    "zh-Hans":"全部",
+    "en":"All"
+  },
+  "option_first_little":{
+    "ja":"はじめて・ちょこっと",
+    "zh-Hant":"初次、少許",
+    "zh-Hans":"初次、少许",
+    "en":"First & Little"
+  },
+  "option_standard":{
+    "ja":"すたんだーど",
+    "zh-Hant":"標準",
+    "zh-Hans":"标准",
+    "en":"Standard"
+  },
+  "option_gorgeous":{
+    "ja":"ごーじゃす",
+    "zh-Hant":"豪華",
+    "zh-Hans":"豪华",
+    "en":"Gorgeous"
+  }
+}
+
+const $buttons = {
+  "#last_delete_text":{
+    "ja":"最後尾削除",
+    "zh-Hant":"刪除末尾",
+    "zh-Hans":"删除末尾",
+    "en":"Delete Last"
+  },
+  "#all_delete_text":{
+    "ja":"全削除",
+    "zh-Hant":"刪除全部",
+    "zh-Hans":"删除全部",
+    "en":"Delete All"
+  },
+  "#EmissionRate_text":{
+    "ja":"排出率算出",
+    "zh-Hant":"計算概率",
+    "zh-Hans":"计算概率",
+    "en":"Calculate Drop Rate"
+  },
+  "#share_text":{
+    "ja":"共有",
+    "zh-Hant":"分享",
+    "zh-Hans":"分享",
+    "en":"Share"
+  },
+  "#itrem_reselect_text":{
+    "ja":"選択解除",
+    "zh-Hant":"取消選擇",
+    "zh-Hans":"取消选择",
+    "en":"Deselect"
+  }
+}
+
+const $iconsImages = {
+  "#l5":{"ja":"ラッキーメダル5","zh-Hant":"幸運獎牌5","zh-Hans":"幸运奖牌5","en":"Lucky Medal 5"},
+  "#l250":{"ja":"ラッキーメダル250","zh-Hant":"幸運獎牌250","zh-Hans":"幸运奖牌250","en":"Lucky Medal 250"},
+  "#o1":{"ja":"オシャレメダル","zh-Hant":"時尚獎牌","zh-Hans":"时尚奖牌","en":"Fashion Medal"},
+  "#kg5":{"ja":"輝きの欠片5","zh-Hant":"閃耀碎片5","zh-Hans":"闪耀碎片5","en":"Sparkle Shard 5"},
+  "#kg10":{"ja":"輝きの欠片10","zh-Hant":"閃耀碎片10","zh-Hans":"闪耀碎片10","en":"Sparkle Shard 10"},
+  "#kg30":{"ja":"輝きのかけら30","zh-Hant":"閃耀碎片30","zh-Hans":"闪耀碎片30","en":"Sparkle Shard 30"},
+  "#msr1":{"ja":"おもいでの石SR","zh-Hant":"回憶之石SR","zh-Hans":"回忆之石SR","en":"Memory Stone SR"},
+  "#mssr1":{"ja":"おもいでの石SSR","zh-Hant":"回憶之石SSR","zh-Hans":"回忆之石SSR","en":"Memory Stone SSR"},
+  "#g1000":{"ja":"ゴールド1000","zh-Hant":"金幣1000","zh-Hans":"金币1000","en":"Gold 1000"},
+  "#g5000":{"ja":"ゴールド5000","zh-Hant":"金幣5000","zh-Hans":"金币5000","en":"Gold 5000"},
+  "#d201":{"ja":"スタミナ20回復ドリンク","zh-Hant":"體力20回復飲","zh-Hans":"体力20回复饮","en":"20 Stamina Recovery Drink"},
+  "#d501":{"ja":"スタミナ50回復ドリンク","zh-Hant":"體力50回復飲","zh-Hans":"体力50回复饮","en":"50 Stamina Recovery Drink"},
+  "#jm10":{"ja":"ジャパまん（オール）中10","zh-Hant":"加帕里饅頭（全）中10","zh-Hans":"加帕里馒头（全）中10","en":"Japari Bun (All) Mideum 10"},
+  "#jb10":{"ja":"ジャパまん（オール）大10","zh-Hant":"加帕里饅頭（全）大10","zh-Hans":"加帕里馒头（全）大10","en":"Japari Bun (All) Large 10"},
+  "#jb30":{"ja":"ジャパまん（オール）大30","zh-Hant":"加帕里饅頭（全）大30","zh-Hans":"加帕里馒头（全）大30","en":"Japari Bun (All) Large 30"},
+  "#jb120":{"ja":"ジャパまん（オール）大120","zh-Hant":"加帕里饅頭（全）大120","zh-Hans":"加帕里馒头（全）大120","en":"Japari Bun (All) Large 120"},
+  "#b1":{"ja":"ジャパリパン","zh-Hant":"加帕里麵包","zh-Hans":"加帕里面包","en":"Japari Bread"},
+  "#dsr1":{"ja":"虹色のアニマルラムネSR","zh-Hant":"虹彩色的動物彈珠汽水SR","zh-Hans":"虹彩色的动物弹珠汽水SR","en":"Rainbow-Colored Animal Ramune SR"},
+  "#dssr1":{"ja":"虹色のアニマルラムネSSR","zh-Hant":"虹彩色的動物彈珠汽水SSR","zh-Hans":"虹彩色的动物弹珠汽水SSR","en":"Rainbow-Colored Animal Ramune SSR"},
+  "#k4":{"ja":"キラキラ4","zh-Hant":"閃亮亮4","zh-Hans":"闪亮亮4","en":"Kirakira 4"},
+  "#k12":{"ja":"キラキラ12","zh-Hant":"閃亮亮12","zh-Hans":"闪亮亮12","en":"Kirakira 12"},
+  "#k25":{"ja":"キラキラ25","zh-Hant":"閃亮亮25","zh-Hans":"闪亮亮25","en":"Kirakira 25"},
+  "#k50":{"ja":"キラキラ50","zh-Hant":"閃亮亮50","zh-Hans":"闪亮亮50","en":"Kirakira 50"},
+  "#k75":{"ja":"キラキラ75","zh-Hant":"閃亮亮75","zh-Hans":"闪亮亮75","en":"Kirakira 75"},
+  "#k100":{"ja":"キラキラ100","zh-Hant":"閃亮亮100","zh-Hans":"闪亮亮100","en":"Kirakira 100"},
+  "#k150":{"ja":"キラキラ150","zh-Hant":"閃亮亮150","zh-Hans":"闪亮亮150","en":"Kirakira 150"},
+  "#k250":{"ja":"キラキラ250","zh-Hant":"閃亮亮250","zh-Hans":"闪亮亮250","en":"Kirakira 250"},
+  "#k300":{"ja":"キラキラ300","zh-Hant":"閃亮亮300","zh-Hans":"闪亮亮300","en":"Kirakira 300"},
+  "#k500":{"ja":"キラキラ500","zh-Hant":"閃亮亮500","zh-Hans":"闪亮亮500","en":"Kirakira 500"},
+  "#k1000":{"ja":"キラキラ1000","zh-Hant":"閃亮亮1000","zh-Hans":"闪亮亮1000","en":"Kirakira 1000"},
+  "#i1":{"ja":"しょうたい券1","zh-Hant":"招待券1","zh-Hans":"招待券1","en":"Invitation Ticket 1"},
+  "#i10":{"ja":"しょうたい券10","zh-Hant":"招待券10","zh-Hans":"招待券10","en":"Invitation Ticket 10"},
+  "#s21":{"ja":"☆2以上フレンズしょうたい券","zh-Hant":"☆2以上朋友招待券","zh-Hans":"☆2以上朋友招待券","en":"2&#42;&#43; Friend Invitation Ticket"},
+  "#s31":{"ja":"☆3以上しょうたい券","zh-Hant":"☆3以上招待券","zh-Hans":"☆3以上招待券","en":"3&#42;&#43; Invitation Ticket"},
+  "#sf31":{"ja":"☆3以上フレンズしょうたい券","zh-Hant":"☆3以上朋友招待券","zh-Hans":"☆3以上朋友招待券","en":"3&#42;&#43; Friend Invitation Ticket"},
+  "#s41":{"ja":"☆4しょうたい券","zh-Hant":"☆4招待券","zh-Hans":"☆4招待券","en":"4&#42; Invitation Ticket"},
+  "#sft41":{"ja":"☆4フレンズしょうたいチケット","zh-Hant":"☆4朋友招待券碎片","zh-Hans":"☆4朋友招待券碎片","en":"4&#42; Friend Invitation Ticket Shard"},
+  "#sf41":{"ja":"☆4フレンズしょうたい券","zh-Hant":"☆4朋友招待券","zh-Hans":"☆4朋友招待券","en":"4&#42; Friend Invitation Ticket"},
+  "#s0":{"ja":"空白","zh-Hant":"空白","zh-Hans":"空白","en":"Space"}
+}
+
+const $multiItems = {
+  "l5":{"ja":"ラッキーメダル","zh-Hant":"幸運獎牌","zh-Hans":"幸运奖牌","en":"Lucky Medal"},
+  "l250":{"ja":"ラッキーメダル","zh-Hant":"幸運獎牌","zh-Hans":"幸运奖牌","en":"Lucky Medal"},
+  "kg5":{"ja":"輝きの欠片","zh-Hant":"閃耀碎片","zh-Hans":"闪耀碎片","en":"Sparkle Shard"},
+  "kg10":{"ja":"輝きの欠片","zh-Hant":"閃耀碎片","zh-Hans":"闪耀碎片","en":"Sparkle Shard"},
+  "kg30":{"ja":"輝きの欠片","zh-Hant":"閃耀碎片","zh-Hans":"闪耀碎片","en":"Sparkle Shard"},
+  "g1000":{"ja":"ゴールド","zh-Hant":"金幣","zh-Hans":"金币","en":"Gold"},
+  "g5000":{"ja":"ゴールド","zh-Hant":"金幣","zh-Hans":"金币","en":"Gold"},
+  "jb10":{"ja":"ジャパまん（オール）大","zh-Hant":"加帕里饅頭（全）大","zh-Hans":"加帕里馒头（全）大","en":"Japari Bun (All) Large"},
+  "jb30":{"ja":"ジャパまん（オール）大","zh-Hant":"加帕里饅頭（全）大","zh-Hans":"加帕里馒头（全）大","en":"Japari Bun (All) Large"},
+  "jb120":{"ja":"ジャパまん（オール）大","zh-Hant":"加帕里饅頭（全）大","zh-Hans":"加帕里馒头（全）大","en":"Japari Bun (All) Large"},
+  "k4":{"ja":"キラキラ","zh-Hant":"閃亮亮","zh-Hans":"闪亮亮","en":"Kirakira"},
+  "k12":{"ja":"キラキラ","zh-Hant":"閃亮亮","zh-Hans":"闪亮亮","en":"Kirakira"},
+  "k25":{"ja":"キラキラ","zh-Hant":"閃亮亮","zh-Hans":"闪亮亮","en":"Kirakira"},
+  "k50":{"ja":"キラキラ","zh-Hant":"閃亮亮","zh-Hans":"闪亮亮","en":"Kirakira"},
+  "k75":{"ja":"キラキラ","zh-Hant":"閃亮亮","zh-Hans":"闪亮亮","en":"Kirakira"},
+  "k100":{"ja":"キラキラ","zh-Hant":"閃亮亮","zh-Hans":"闪亮亮","en":"Kirakira"},
+  "k150":{"ja":"キラキラ","zh-Hant":"閃亮亮","zh-Hans":"闪亮亮","en":"Kirakira"},
+  "k250":{"ja":"キラキラ","zh-Hant":"閃亮亮","zh-Hans":"闪亮亮","en":"Kirakira"},
+  "k300":{"ja":"キラキラ","zh-Hant":"閃亮亮","zh-Hans":"闪亮亮","en":"Kirakira"},
+  "k500":{"ja":"キラキラ","zh-Hant":"閃亮亮","zh-Hans":"闪亮亮","en":"Kirakira"},
+  "k1000":{"ja":"キラキラ","zh-Hant":"閃亮亮","zh-Hans":"闪亮亮","en":"Kirakira"},
+  "i1":{"ja":"しょうたい券","zh-Hant":"招待券","zh-Hans":"招待券","en":"Invitation Ticket"},
+  "i10":{"ja":"しょうたい券","zh-Hant":"招待券","zh-Hans":"招待券","en":"Invitation Ticket"},
+}
+
+const $tablesTh = {
+  "#th_image":{
+    "ja":"画像",
+    "zh-Hant":"圖片",
+    "zh-Hans":"图片",
+    "en":"image"
+  },
+  "#th_itemName":{
+    "ja":"アイテム名",
+    "zh-Hant":"物品名",
+    "zh-Hans":"物品名",
+    "en":"Item Name"
+  },
+  "#th_count":{
+    "ja":"個数",
+    "zh-Hant":"數量",
+    "zh-Hans":"数量",
+    "en":"Quantity"
+  },
+  "#th_EmissionRate":{
+    "ja":"排出率",
+    "zh-Hant":"概率",
+    "zh-Hans":"概率",
+    "en":"Drop Rate"
+  }
+}
+
+const $selecters = {"ja": "選択してください", "zh-Hant": "請選擇", "zh-Hans":"请选择", "en":"Please Select"}
+
+const $shareContent = {
+  "ja": "スペシャルデイリーボーナスの結果です！",
+  "zh-Hant": "特別每日任務獎勵的結果！",
+  "zh-Hans":"特别每日任务奖励的结果！",
+  "en":"Special Daily Bonus Results!"
+}
+const $total = {"ja": "合計", "zh-Hant":"總計","zh-Hans":"总计", "en":"Total"}
+const $daysTotal = {"ja": "アイコン数", "zh-Hant":"物品數量", "zh-Hans":"物品数量", "en":"Number of Icons"}
+const $acquisitionsCount = {"ja": "獲得数", "zh-Hant":"獲得數量", "zh-Hans":"获得数量", "en":" Total"}
